0252f333d20e2a28e289e9a22ab90e355640ab9f
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2023-10-26 21:33:57 +0500

    crud api done

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,7 +3,7 @@ const restaurantModel = require("../model/restaurant");
 module.exports = {
   Query: {
     // it takes 4 parameters
-    // first is parent so here we dono have parent
+    // first is parent so here we donot have parent
     async restaurant(_, { ID }) {
       const data = await restaurantModel.findOne({ _id: ID });
       return data;
@@ -25,9 +25,33 @@ module.exports = {
         location: location,
       });
 
-      await createdRestaurant.save()
+      await createdRestaurant.save();
 
       return createdRestaurant;
+    },
+
+    async deleteRestaurant(_, { ID }) {
+      const wasDeleted = (await restaurantModel.deleteOne({ _id: ID }))
+        .deletedCount;
+      return wasDeleted; //1 if something deleted and 0 if nothing deleted
+    },
+
+    async editRestaurant(
+      _,
+      { ID, RestaurantInput: { restaurantName, country, city, location } }
+    ) {
+      const wasEdited = (
+        await restaurantModel.updateOne(
+          { _id: ID },
+          {
+            restaurantName: restaurantName,
+            country: country,
+            city: city,
+            location: location,
+          }
+        )
+      ).modifiedCount;
+      return wasEdited;
     },
   },
 };
